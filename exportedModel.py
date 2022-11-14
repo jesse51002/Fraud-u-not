47c57e27fb856d214ac6cfdd988c8d4fdbc4a100
Jesse Musa <jessemusa2@gmail.com>
2022-11-14 12:06:46 -0600

    craete backedn branch

---
+++ exportedModel.py
@@ -0,0 +1,63 @@
+import pickle
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+
+randForestModel = pickle.load(open('./exportModel.pkl', 'rb'))
+
+
+class modelPrediction:
+
+    def  __init__(self, distanceFromHome=0, distanceFromLastTran=0, priceRatio=1, repeatRetail=1, usedChip=0, usedPin=0, onlineOrder=0):
+        self.dataFrame = pd.DataFrame(columns=['distance_from_home', 'distance_from_last_transaction',
+       'ratio_to_median_purchase_price', 'repeat_retailer', 'used_chip',
+       'used_pin_number', 'online_order'])
+        
+        self.dataFrame['distance_from_home'] = [distanceFromHome]
+        self.dataFrame['distance_from_last_transaction'] = [distanceFromLastTran]
+        self.dataFrame['ratio_to_median_purchase_price'] = [priceRatio]
+        self.dataFrame['repeat_retailer'] = [repeatRetail]
+        self.dataFrame['used_chip'] = [usedChip]
+        self.dataFrame['used_pin_number'] = [usedPin]
+        self.dataFrame['online_order'] = [onlineOrder]
+
+        self.__featureEnginnering()
+
+
+    def __featureEnginnering(self):
+
+        def numericTrans(colName ,ranges, newName):
+            # stores data
+            newData = []
+            for index,x in self.dataFrame.iterrows():
+                # Gets the value at the row
+                curVal = x[colName]
+                # stores the encoded val in the range
+                encodedVal = len(ranges) - 1
+                # Loops throught hte range
+                for i in range(len(ranges) - 2, -1, -1):
+                    # if curval is less than then this value is closer to the target value
+                    if curVal <= ranges[i]:
+                        encodedVal = i
+                    # other wise it's not close to break
+                    else:
+                        break
+                # Adds the encoded value to the data arr
+                newData.append(encodedVal)
+            # Adds the data to the panda dataframe
+            self.dataFrame[newName] = newData
+    
+        valRanges = {
+            'distance_from_home': [4, 10, 25, 50, 75, 100, 125, 150,250,1000, 5000],
+            'distance_from_last_transaction': [0.3, 1, 3, 10, 40, 60, 70, 80, 100, 250, 500, 1000, 5000],
+            'ratio_to_median_purchase_price': [0.5, 1, 3, 5, 7.5, 10, 35, 60, 100, 250, 500]
+        }
+    
+        for name, ranges in valRanges.items():
+            numericTrans(name, ranges, name + "_encoded")
+    
+    def predictFraud(self):
+        return randForestModel.predict(self.dataFrame)[0]
+
+
+
+
---
+++ server.py
@@ -0,0 +1,38 @@
+#python server.py to start the server
+
+from flask import Flask, request
+
+from exportedModel import modelPrediction
+
+app = Flask(__name__)
+
+# Member API rooute
+
+@app.route('/fraudPred', methods=['POST', 'GET'])
+
+def fraudPred():
+    # Gets the request data
+    reqData = request.get_json()
+
+
+    # Instantiates the model prediction class
+    predClass = modelPrediction(
+        distanceFromHome= reqData['DistanceFromHome'], 
+        distanceFromLastTran= reqData['DistanceFromLastPurchase'], 
+        priceRatio= reqData['Amount'] / getAveragePurchase(reqData['Username']), 
+        repeatRetail=reqData['RepeatRetail'], 
+        usedChip=reqData['UsedChip'], 
+        usedPin=reqData['UsedPin'], 
+        onlineOrder=reqData['OnlineOrder']
+    )
+
+    # Sends the prediction back to the front end
+    return {"FraudPrediction": predClass.predictFraud()}
+
+# Will update this to get purchase data from database
+def getAveragePurchase(username):
+    return 50
+
+# Runs the app
+if __name__ == '__main__':
+    app.run(debug=True)
