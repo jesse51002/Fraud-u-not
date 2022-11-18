8038e0879c962ee37e75e57e37bc2e354067d0fe
ssk10000 <91851299+ssk10000@users.noreply.github.com>
2022-11-18 12:05:31 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ db = mysql.connector.connect(
     host="localhost",
     user="root",
     #insertpassword
-    passwd="insertyourpassword",
-    database="fraudunot"
+    passwd="",
+    database="fraudunotproject"
 )
 mycursor = db.cursor(buffered=True)
 #run mysql once
@@ -15,7 +15,7 @@ mycursor = db.cursor(buffered=True)
 #gets persons details
 
 def getdetailssqlfunction(personfname):
-    sql = "SELECT * FROM accounts WHERE firstname = '" + personfname + "'";
+    sql = "SELECT * FROM accountscustomers WHERE firstname = '" + personfname + "'";
     print(sql)
     mycursor.execute(sql)
 
@@ -23,26 +23,33 @@ def getdetailssqlfunction(personfname):
 
 #TEST
 
-#getdetailssqlfunction('sujay')
+getdetailssqlfunction('sujay')
 
 #inserts persons details
 
 def insertpersonfunction(firstname, lastname, username, password3, email,phone, City, State, Country, bank, banksemail,banksphonenumber, associatedcard, accountnum, routingnum):
-    sql = "Insert INTO accounts (firstname, lastname, username, password3, email,phone, City, State, Country, bank, banksemail,banksphonenumber, associatedcard, accountnum, routingnum) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    sql = "Insert INTO accountscutomers (firstname, lastname, username, password3, email,phone, City, State, Country, bank, banksemail,banksphonenumber, associatedcard, accountnum, routingnum) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
     val = (firstname,lastname,username,password3,email,phone,City,State,Country,bank,banksemail,banksphonenumber,associatedcard,accountnum,routingnum)
     print(sql)
     mycursor.execute(sql,val)
+    sql = "CREATE TABLE IF NOT EXISTS " + firstname + "purchaseshistory(purchasedate TEXT,purchaseamount TEXT,purchaseemail TEXT);"
+    print(sql)
+    mycursor.execute(sql)
+
 
 #TEST
 
 #insertpersonfunction('darth','vader','sith','palp','email','9899899898','Alderran','Coruscant','tatooine','chase','email','8888888888','credit','890543','980343')
 #getdetailssqlfunction('darth')
 
+
 #deletes a person from database
 
 
 def deleteperson(fname):
-    sql = "DELETE FROM accounts WHERE firstname = " + "'" +fname +"'"
+    sql = "DELETE FROM accountscustomers WHERE firstname = " + "'" +fname +"'"
+    mycursor.execute(sql)
+    sql = "DROP TABLE " + fname + "purchaseshistory"
     mycursor.execute(sql)
 
 #TEST
@@ -51,7 +58,7 @@ def deleteperson(fname):
 #getdetailssqlfunction('darth')
 
 def updatepersnsdetails(varname,varvalue,fname):
-    sql = "UPDATE accounts SET " + varname + " = "  + "'" + varvalue +"'" + " WHERE firstname = " + "'" + fname + "'"
+    sql = "UPDATE accountscustomers SET " + varname + " = "  + "'" + varvalue +"'" + " WHERE firstname = " + "'" + fname + "'"
     print(sql)
     mycursor.execute(sql)
 
@@ -60,5 +67,20 @@ def updatepersnsdetails(varname,varvalue,fname):
 #updatepersnsdetails('lastname','karanam','sujay')
 #getdetailssqlfunction('sujay')
 
+def getpaymentdetails(fname):
+    sql = "SELECT * FROM " + fname + "purchaseshistory;"
+    mycursor.execute(sql)
+
+def insertpaymentdetail(fname,purchasedate,purchaseamount):
+    sql = "INSERT INTO " + fname + "purchaseshistory(purchasedate,purchaseamount)VALUES(%s,%s);"
+    val = (purchasedate,purchaseamount)
+    print(sql)
+    mycursor.execute(sql,val)
+
+#TEST
+
+#insertpaymentdetail('darth','August 6, 4545','79$')
+#deleteperson('darth')
+#getpaymentdetails('darth')
 for x in mycursor:
     print(x)
